f590995175aefa1f73b41d6be582182e3084f6cc
leejunnyeop <daffodil8620@naver.com>
2025-07-05 18:28:19 +0900

    feat: Kakao 유저 정보 받기 구현
    - KakaoUserResponse: 이메일, 닉네임, 프로필 이미지 필드 정의
    - KakaoUserProfileDto로 변환하는 KakaoMapper 유틸 추가

---
+++ src/main/java/com/example/soso/kakao/dto/KakaoUserResponse.java
@@ -0,0 +1,22 @@
+package com.example.soso.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KakaoUserResponse(
+        Long id,
+
+        @JsonProperty("kakao_account")
+        KakaoAccount kakaoAccount
+) {
+    public record KakaoAccount(
+            Profile profile,
+            String email
+    ) {
+        public record Profile(
+                String nickname,
+
+                @JsonProperty("profile_image_url")
+                String profileImageUrl
+        ) {}
+    }
+}
