ad3e2c5ffb4282f4359776ff1f20294fe868335f
leejunnyeop <daffodil8620@naver.com>
2025-07-05 18:29:33 +0900

    feat: FeignConfig 구현
    - 공통 RequestInterceptor 등록
    - User-Agent 및 Authorization 헤더 설정 구조 포함

---
+++ src/main/java/com/example/soso/global/config/FeignConfig.java
@@ -0,0 +1,10 @@
+package com.example.soso.global.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.example.soso")
+public class FeignConfig {
+
+}
