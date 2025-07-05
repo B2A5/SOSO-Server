f4ae99179229f266188a29f52c95748248afc64d
leejunnyeop <daffodil8620@naver.com>
2025-07-05 18:35:59 +0900

    feat: 사용자 도메인 및 리포지토리 구성
    - Users 엔티티 정의 (username, email, role, gender 등 포함)
    - Roles, Gender enum 정의
    - UserRepository 인터페이스 생성

---
+++ src/main/java/com/example/soso/users/domain/entity/Users.java
@@ -0,0 +1,62 @@
+package com.example.soso.users.domain.entity;
+
+import com.example.soso.global.timeEntity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Users extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String nickName;
+
+    private String phoneNumber;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Roles role;
+
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private LocalDate birthDate;
+
+    private String location;
+
+    private String latitude;
+    private String longitude;
+
+    @Builder
+    public Users(String username, String nickName, String phoneNumber, String email, String profileImageUrl, Roles role,
+                 Gender gender, LocalDate birthDate, String location, String latitude, String longitude) {
+        this.username = username;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.profileImageUrl = profileImageUrl;
+        this.role = role;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
