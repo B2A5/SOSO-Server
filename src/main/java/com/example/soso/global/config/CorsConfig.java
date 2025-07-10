3ce7e261610765a7c065b9f4227656d9335e080e
leejunnyeop <daffodil8620@naver.com>
2025-07-10 13:22:26 +0900

    refactor :cors 환경 변수로 설정

--- src/main/java/com/example/soso/global/config/CorsConfig.java
+++ src/main/java/com/example/soso/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.soso.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.*;
@@ -9,11 +10,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.origin}")
+    private String frontOrigin;
+
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://soso-front-end-web.vercel.app")); //  프론트 도메인
+        config.setAllowedOrigins(List.of(frontOrigin)); //  프론트 도메인
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
