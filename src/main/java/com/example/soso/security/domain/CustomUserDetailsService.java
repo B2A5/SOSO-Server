ce29280d95c1a4988c29af83c2bf1bdb21eddbb3
leejunnyeop <daffodil8620@naver.com>
2025-07-12 17:33:01 +0900

    refactor :  CustomUserDetails 타입 변경
    - CustomUserDetailsService 구현: 사용자 ID 기반으로 Users 조회

--- src/main/java/com/example/soso/security/domain/CustomUserDetailsService.java
+++ src/main/java/com/example/soso/security/domain/CustomUserDetailsService.java
@@ -2,7 +2,9 @@ package com.example.soso.security.domain;
 
 import com.example.soso.global.exception.domain.UserErrorCode;
 import com.example.soso.global.exception.util.UserAuthException;
+import com.example.soso.users.domain.entity.Users;
 import com.example.soso.users.repository.UsersRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,10 +20,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
 
-        if (userId == null || userId.isBlank() || !userRepository.existsById(userId)) {
-            throw new UserAuthException(UserErrorCode.UNAUTHORIZED_USER);
-        }
+        Users user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserAuthException(UserErrorCode.UNAUTHORIZED_USER));
 
-        return new CustomUserDetails(userId);
+        return new CustomUserDetails(user);
     }
 }
