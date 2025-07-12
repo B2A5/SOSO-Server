96f6b6f922f026e98c4d889dec26a45fa88ae035
leejunnyeop <daffodil8620@naver.com>
2025-07-12 17:47:21 +0900

    chore : 단순 수정

--- src/main/java/com/example/soso/global/config/S3Service.java
+++ src/main/java/com/example/soso/global/config/S3Service.java
@@ -1,6 +1,5 @@
 package com.example.soso.global.config;
 
-import com.example.soso.global.config.ImageResizeUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
