b02fd317cd1da98e6fd413c614a92dd6686a2a0f
leejunnyeop <daffodil8620@naver.com>
2025-07-28 17:57:30 +0900

    fix : 카카오 로그인시 프로필 이미지를 선택하지 않으면 해당 오류

--- src/main/java/com/example/soso/kakao/mapper/KakaoMapper.java
+++ src/main/java/com/example/soso/kakao/mapper/KakaoMapper.java
@@ -6,10 +6,18 @@ import com.example.soso.kakao.dto.KakaoUserResponse;
 public class KakaoMapper {
 
     public static KakaoUserProfileDto from(KakaoUserResponse kakaoUser) {
-        return new KakaoUserProfileDto(
-                "kakao_" + kakaoUser.id(),
-                kakaoUser.kakaoAccount().email(),
-                kakaoUser.kakaoAccount().profile().profileImageUrl()
-        );
+        String id = "kakao_" + kakaoUser.id();
+        String email = null;
+        String profileImageUrl = null;
+
+        if (kakaoUser.kakaoAccount() != null) {
+            email = kakaoUser.kakaoAccount().email();
+
+            var profile = kakaoUser.kakaoAccount().profile();
+            if (profile != null) {
+                profileImageUrl = profile.profileImageUrl();
+            }
+        }
+        return new KakaoUserProfileDto(id, email, profileImageUrl);
     }
 }
