f723652219b610de178ffe060e3573209ef65d13
leejunnyeop <daffodil8620@naver.com>
2025-07-28 12:10:12 +0900

    feat (comment): 댓글 조회 응답 및 엔드포인 구현

---
+++ src/main/java/com/example/soso/comment/domain/dto/PostCommentResponse.java
@@ -0,0 +1,23 @@
+package com.example.soso.comment.domain.dto;
+
+import com.example.soso.post.domain.dto.UserSummaryResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "댓글 응답 DTO")
+public record PostCommentResponse(
+
+        @Schema(description = "댓글 ID", example = "301")
+        Long id,
+
+        @Schema(description = "댓글 내용", example = "저도 봤어요! 정말 유익했어요.")
+        String content,
+
+        @Schema(description = "해당 댓글에 좋아요 수", example = "5")
+        int likeCount,
+
+        @Schema(description = "작성일시", example = "2025-07-26T12:34:56")
+        String createdAt,
+
+        @Schema(description = "작성자 정보")
+        UserSummaryResponse user
+) {}
